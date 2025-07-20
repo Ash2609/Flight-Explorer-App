7c8fa0be7480bfb6ec8b25a8dd8ad1bec27d605f
Ash2609 <70098228+Ash2609@users.noreply.github.com>
2025-07-21 00:19:47 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { encryptPassword } from "../utils/crypto";
 import FlightLogo from "../assets/Flight-logo.jpg";
+import {Link} from "react-router-dom";
 
 
 const Signup = () => {
